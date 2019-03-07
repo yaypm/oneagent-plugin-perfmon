11850d2bcdbaa033da269eb3e7ac109a275ba931
yaypm <aliemslie@gmail.com>
2019-03-07 17:00:43 +0000

    Update msmq.py

--- msmq.py
+++ msmq.py
@@ -13,8 +13,8 @@ class MsmqPlugin(BasePlugin):
         pgi = self.find_single_process_group(pgi_name('Windows System'))
         pgi_id = pgi.group_instance_id
 
-        staffdetail = winstats.get_perf_data(r'\MSMQ Queue(<hostname>\private$\,<queuename>)\Messages in Queue', fmts='double', delay=100)
-        line500badaccount_uk = winstats.get_perf_data(r'\MSMQ Queue(<hostname>\private$\<queuename>)\Messages in Queue', fmts='double', delay=100)
+        queue1 = winstats.get_perf_data(r'\MSMQ Queue(<hostname>\private$\<queuename>)\Messages in Queue', fmts='double', delay=100)
+        queue2 = winstats.get_perf_data(r'\MSMQ Queue(<hostname>\private$\<queuename>)\Messages in Queue', fmts='double', delay=100)
 
         self.results_builder.relative(key='<queuename>', value=<queuename>[0], entity_id=pgi_id)
         self.results_builder.relative(key='<queuename>', value=<queuename>[0], entity_id=pgi_id)
